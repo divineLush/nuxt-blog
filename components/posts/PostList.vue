b8b0bd8b9c2ab9a333b1dcb34e5a36d018dcd66e
divineLush <homeoflucifer@gmail.com>
2021-03-08 21:01:22 +0700

    asyncData

--- components/posts/PostList.vue
+++ components/posts/PostList.vue
@@ -22,6 +22,32 @@ export default {
         PostPreview,
     },
 
+    // runs on the server
+    // waits for the asynchronous task to finish and then returns a page
+    // gotta return a value
+    // 'this' doesn't work as expected
+    // gotta return a promise or execute a callback method
+    // so asyncData has the idea when the work is finished
+    // asyncData(context, callback) {
+    //     console.log(context, callback, this);
+    //     setTimeout(() => {
+    //         // gotta pas an error or null as a first argument
+    //         // and object that will become a 'data' property
+    //         callback(null, { posts });
+    //     }, 500);
+    // },
+
+    asyncData(context) {
+        console.log(context);
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                res({ posts });
+            }, 500);
+        })
+        .then(data => data)
+        .catch(error => context.error(new Error()));
+    },
+
     props: {
         isAdmin: {
             type: Boolean,
@@ -29,11 +55,14 @@ export default {
         },
     },
 
-    computed: {
-        posts() {
-            return posts;
-        }
-    }
+    // shouldn't use 'posts' inside 'data'
+    // as this code runs on the client
+    // and will override asyncData
+    // data() {
+    //     return {
+    //         posts: [],
+    //     }
+    // },
 }
 </script>
 
