20a74a0bd5f41637794a9e92a3e41f867e895baf
divineLush <homeoflucifer@gmail.com>
2021-03-08 21:51:29 +0700

    nuxtServerInit

--- components/posts/PostList.vue
+++ components/posts/PostList.vue
@@ -13,7 +13,7 @@
 
 <script>
 import PostPreview from '~/components/posts/PostPreview.vue';
-import posts from '~/assets/js/posts';
+// import posts from '~/assets/js/posts';
 
 export default {
     name: 'PostList',
@@ -22,31 +22,16 @@ export default {
         PostPreview,
     },
 
-    // runs on the server
-    // waits for the asynchronous task to finish and then returns a page
-    // gotta return a value
-    // 'this' doesn't work as expected
-    // gotta return a promise or execute a callback method
-    // so asyncData has the idea when the work is finished
-    // asyncData(context, callback) {
-    //     console.log(context, callback, this);
-    //     setTimeout(() => {
-    //         // gotta pas an error or null as a first argument
-    //         // and object that will become a 'data' property
-    //         callback(null, { posts });
-    //     }, 500);
-    // },
+    // async fetch(content) {
+    //     const loadedPosts = await new Promise(res => {
+    //         setTimeout(() => {
+    //             res(posts);
+    //         }, 500);
+    //     })
 
-    asyncData(context) {
-        console.log(context);
-        return new Promise((res, rej) => {
-            setTimeout(() => {
-                res({ posts });
-            }, 500);
-        })
-        .then(data => data)
-        .catch(error => context.error(new Error()));
-    },
+    //     console.log(context);
+    //     this.posts = loadedPosts;
+    // },
 
     props: {
         isAdmin: {
@@ -55,14 +40,11 @@ export default {
         },
     },
 
-    // shouldn't use 'posts' inside 'data'
-    // as this code runs on the client
-    // and will override asyncData
-    // data() {
-    //     return {
-    //         posts: [],
-    //     }
-    // },
+    data() {
+        return {
+            posts: [],
+        }
+    },
 }
 </script>
 
---
+++ store/index.js
@@ -0,0 +1,36 @@
+import Vuex from 'vuex';
+import posts from '~/assets/js/posts';
+
+// gotta create a function instead of just object
+// as it should be callable by nuxt
+// which will execute it on the server
+// if the object is returned, all users will get the same instance
+const createStore = () => new Vuex.Store({
+    state: {
+        posts: [],
+    },
+    mutations: {
+        setPosts(state, posts) {
+            state.posts = posts;
+        }
+    },
+    actions: {
+        // will be dispatched by nuxt
+        // initialize store
+        // nuxtServerInit(vuexContext, context)
+        nuxtServerInit({ commit }) {
+            commit('setPosts', posts);
+        },
+
+        setPosts(vuexContext, posts) {
+            vuexContext.commit('setPosts', posts);
+        }
+    },
+    getters: {
+        posts(state) {
+            return state.posts;
+        },
+    },
+});
+
+export default createStore;
